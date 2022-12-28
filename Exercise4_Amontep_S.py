de425a5674a5ae72f0e996b2beef4281d09b0d84
Randsven <suntarin@outlook.com>
2022-12-28 18:26:41 +0700

    Create Exercise4_Amontep_S.py

---
+++ Exercise4_Amontep_S.py
@@ -0,0 +1,10 @@
+FE = 90
+GB = 89
+ICS = 90
+CP = 100
+print('--- Your Score---')
+print('Foundation English :',FE)
+print('General Business :',GB)
+print('Introduction to :',ICS)
+print('Computer Programming :',CP)
+print('..----------------------------..')
